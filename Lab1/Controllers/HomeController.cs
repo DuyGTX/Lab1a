7497e5253605867090b5debea1ccbac52ec52d81
DUY <DUY@LAPTOP-KUN>
2023-09-19 00:05:29 +0700

    Add project files.

---
+++ Lab1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Lab1()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Lab1a_2()
+        {
+
+
+            return View();
+        }
+        public ActionResult Lab1a_3()
+        {
+            return View();
+        }
+    }
+}
